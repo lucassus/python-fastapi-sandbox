70f1e212f64e4eb6f64954a07b5a885205820aa0
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-01-28 14:53:09 +0100

    Use get_project dependency everywhere

--- todos/routes/projects.py
+++ todos/routes/projects.py
@@ -4,8 +4,7 @@ from fastapi import APIRouter, Depends
 from sqlalchemy.orm import Session
 
 from todos import schemas
-from todos.common.errors import EntityNotFoundError
-from todos.dependencies import get_session
+from todos.dependencies import get_project, get_session
 from todos.entities import Project
 
 router = APIRouter()
@@ -14,17 +13,12 @@ router = APIRouter()
 @router.get(
     "",
     response_model=List[schemas.Project],
-    name="Returns the list of projects",
+    name="Returns the list of all projects",
 )
 async def projects_endpoint(session: Session = Depends(get_session)):
     return session.query(Project).all()
 
 
-@router.get("/{id}", response_model=schemas.Project)
-def project_endpoint(id: int, session: Session = Depends(get_session)):
-    project = session.query(Project).get(id)
-
-    if project is None:
-        raise EntityNotFoundError(detail=f"Unable to find a project with ID={id}")
-
+@router.get("/{project_id}", response_model=schemas.Project)
+def project_endpoint(project: Project = Depends(get_project)):
     return project
