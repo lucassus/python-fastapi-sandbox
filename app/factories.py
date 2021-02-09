bdbde0706cd5593195d13d2ee71e82a463b793ad
Lukasz Bandzarewicz <lukasz.bandzarewicz@gmail.com>
2021-02-09 18:29:25 +0100

    Cleanup factories

--- app/factories.py
+++ app/factories.py
@@ -1,16 +1,8 @@
-from typing import Optional
-
 from app.models import User
 
 
 def build_user(
     *,
-    id: Optional[int] = None,
     email: str = "test@email.com",
 ) -> User:
-    user = User(email=email, password="password")
-
-    if id is not None:
-        user.id = id
-
-    return user
+    return User(email=email, password="password")
